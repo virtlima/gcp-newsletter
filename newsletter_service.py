27d67655900e83a2dfb8ddb9ec405ee0d90baad7
ljogeiger <geiger.ljo@gmail.com>
2024-10-29 17:47:05 -0400

    added feature to display newsletter to ft frontend. But cannot figure out how to make ft scroll

--- newsletter_service.py
+++ newsletter_service.py
@@ -7,74 +7,95 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+"""
+Function to generate a newsletter from RSS feed.
 
-sender_email = os.environ.get("SENDER_EMAIL")
-sender_password = os.environ.get("SENDER_PASSWORD") # Make gmail app password from here:https://support.google.com/accounts/answer/185833?visit_id=638655613103411169-2477840836&p=InvalidSecondFactor&rd=1
+Arguments:
+  - source (string): rss_url source - GCP blog feed by default
+  - time_period (string): either "day" or "week"
+  - user_topic (string): what topic is the user interested in (retail, finance, ai/ml, data, etc.)
+  - user_persona (string): what persona is the user (cxo, dev, ce, etc.)
 
-USER_PERSONA = ["CXO","Dev"]
-USER_INDUSTRY = ["Financial","Retail"]
+Response:
+  - newsletter (string): formatted newsletter with recommendation and all article summaries
+"""
 
-# Define the RSS feed URL - "https://blog.google/products/google-cloud/rss/"
-rss_url = "https://snownews.appspot.com/feed"
 
-# Parse the RSS feed
-ssl._create_default_https_context = ssl._create_unverified_context
-feed = feedparser.parse(rss_url)
+def get_newsletter_from_sources(source="https://snownews.appspot.com/feed",
+                                time_period="day",
+                                user_topic="Any",
+                                user_persona="All"):
 
-# Extract the last week's entries
-last_day_entries = []
-for entry in feed.entries:
-    # Check if the entry was published within the last week
-    published_date = datetime.datetime(*entry.published_parsed[:6])
-    if published_date.date() == datetime.datetime.now().date():
-        last_day_entries.append({
-            "title": entry.title,
-            "link": entry.link,
-            "published": entry.published,
-            "metadata": entry.summary,
-        })
+    # Parse the RSS feed
+    ssl._create_default_https_context = ssl._create_unverified_context
+    feed = feedparser.parse(source)
 
-print(len(last_day_entries))
+    entries = []
+    # Extract the entries depending on time period specified
+    if time_period.lower() == "day":
 
-# Write last week entries to output file
-with open(f"data/last_day_entries.json", "w") as f:
-    json.dump(last_day_entries, f)
+        for entry in feed.entries:
+            # Check if the entry was published within the last week
+            published_date = datetime.datetime(*entry.published_parsed[:6])
+            if published_date.date() == datetime.datetime.now().date():
+                entries.append({
+                    "title": entry.title,
+                    "link": entry.link,
+                    "published": entry.published,
+                    "metadata": entry.summary,
+                })
+    elif time_period.lower() == "week":
+        for entry in feed.entries:
+            # Check if the entry was published within the last week
+            published_date = datetime.datetime(*entry.published_parsed[:6])
+            if published_date.isocalendar().week == datetime.datetime.now(
+            ).isocalendar().week:
+                entries.append({
+                    "title": entry.title,
+                    "link": entry.link,
+                    "published": entry.published,
+                    "metadata": entry.summary,
+                })
+    else:
+        print("Please define time period")
 
-# Generate summaries
-summaries = gemini_wrapper.generate_summaries(last_day_entries)
+    print(len(entries))
 
-# Write summaries to output file
-with open(f"data/summaries.json", "w") as f:
-    json.dump(summaries, f)
+    # Write last week entries to output file
+    with open(f"data/entries.json", "w") as f:
+        json.dump(entries, f)
 
-# Generate recommendations
-rec_json = gemini_wrapper.generate_recommendation(USER_INDUSTRY[0],
-                                                USER_PERSONA[0],
-                                                summaries)
+    # Generate summaries
+    summaries = gemini_wrapper.generate_summaries(entries)
 
-# Write recs to output file
-with open(f"data/recommended_articles.json", "w") as f:
-    json.dump(rec_json, f)
+    # Write summaries to output file
+    with open(f"data/summaries.json", "w") as f:
+        json.dump(summaries, f)
 
-# Format output
-newsletter = f"""\n
-Hi!
+    # Generate recommendations
+    rec_json = gemini_wrapper.generate_recommendation(
+        user_topic=user_topic, user_persona=user_persona, summaries=summaries)
 
-Recs: {rec_json['summary_text']}
+    # Write recs to output file
+    with open(f"data/recommended_articles.json", "w") as f:
+        json.dump(rec_json, f)
 
-""" + "\n".join([f"{i+1}. {item['recommendation_title']}\n\t\
-                 Summary: {item['recommendation_summary']}\n\t\
-                 Reason: {item['recommendation_reason']}\n\t\
-                 Link: {item['recommendation_link']}"
-                 for i, item in enumerate(rec_json["recommendations"])]) + "\n"
+    # Format output
+    rec_string = f"Recs: {rec_json['summary_text']}\n\n" + "".join([
+        f"{i+1}. {rec['recommendation_title']}\n\
+                  Summary: {rec['recommendation_summary']}\n\
+                  Reason: {rec['recommendation_reason']}\n\
+                  Link: {rec['recommendation_link']}\n\n"
+        for i, rec in enumerate(rec_json["recommendations"])
+    ]) + "\n"
+    summary_string = "Complete List of Articles:\n\n" + "".join([
+        f"{j+1}. {summary['title']}\n\
+                  Summary: {summary['summary']}\
+                  Link: {summary['link']}\n\n"
+        for j, summary in enumerate(summaries)
+    ]) + "\n"
+    newsletter = f"""Hi!\n\n""" + rec_string + summary_string
 
-print(newsletter)
+    print(newsletter)
 
-# Send the email
-email_service.send_email(
-    sender_email,
-    sender_password,
-    'lukasgeiger@google.com',
-    'GCP Newsletter',
-    newsletter
-)
+    return newsletter
