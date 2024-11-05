e75fa6ea5afb36e02ac86a8e6f9a27f555070c17
Lukas Geiger <geiger.ljo@gmail.com>
2024-11-04 23:02:53 -0500

    Added formatting for email_service. Using Jinja2 to make email templating easier in the future

--- email_service.py
+++ email_service.py
@@ -1,24 +1,32 @@
-import smtplib
+import smtplib, datetime
 from email.mime.text import MIMEText
+from jinja2 import Environment, FileSystemLoader
 
-def send_email(sender_email, sender_password, receiver_email, subject, body):
-  """Sends an email with the given parameters.
+
+def send_email(sender_email, sender_password, receiver_email, subject,
+               recommendations, summaries):
+    """Sends an email with the given parameters.
 
   Args:
     sender_email: The sender's email address.
     sender_password: The sender's email password.
     receiver_email: The recipient's email address.
     subject: The email subject.
-    body: The email body.
+    recommendations: List of recommended articles.
+    summaries: Full list of articles
   """
-  msg = MIMEText(body)
-  msg['Subject'] = subject
-  msg['From'] = sender_email
-  msg['To'] = receiver_email
+    env = Environment(loader=FileSystemLoader('assets'))
+    template = env.get_template('email_template.html')
 
-  with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-    server.login(sender_email,
-                 sender_password)
-    server.sendmail(sender_email,
-                    receiver_email,
-                    msg.as_string())
+    email_content = template.render(recommended_articles=recommendations,
+                                    all_articles=summaries,
+                                    year=datetime.datetime.now().year)
+
+    msg = MIMEText(email_content, 'html')
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
